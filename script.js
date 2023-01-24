adc27ecbab1983690e0aeec941031fc0ffe0c8e6
bcebel <bcebel@gmail.com>
2023-01-24 15:07:23 -0500

    removed consolelogs

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ const specialChar ="~!@#$%^&*()_+`?/.,><";
 function upperQuestion() {
   if(confirm("Do you want upper case letters in your password?")==true){
     conca=conca.concat(upper);
-    console.log('wombat ' + conca);
   };
 }
 upperQuestion();
@@ -20,7 +19,6 @@ upperQuestion();
 function lowerQuestion() {
   if (confirm("Do you want lower case letters in your password?")==true){
     conca=conca.concat(lower);
-    console.log('squirrel ' + conca);
 
   };
 }
@@ -29,7 +27,6 @@ lowerQuestion();
 function numberQuestion() {
   if (confirm("Do you want numbers in your password?")==true) {
     conca=conca.concat(nums);
-    console.log('nunchucks ' + conca);
    
   };
 }
@@ -38,7 +35,6 @@ numberQuestion();
 function charQuestion() {
   if (confirm("Do you want special characters in your password?")==true){
     conca=conca.concat(specialChar);
-    console.log('scorpions ' + conca);
   }
   };
 charQuestion();
@@ -52,55 +48,14 @@ function writePassword() {
   passwordText.value = password;
 }
 generateBtn.addEventListener("click", writePassword);
-console.log(writePassword +' writePW');
 
 
 function generatePassword (){
-console.log(specialChar[Math.floor(Math.random() * specialChar.length)]);
-console.log(conca);
-//loop this one as it is the password character generator
 let arrray=[];
 for (let i = 0; i < charTotal; i++) {
   arrray.push((conca[Math.floor(Math.random() * conca.length)]));
-    
 }
 var pwdigits=arrray.join("");
-console.log(pwdigits +'yo');
 return pwdigits;
 }
 
-
-
-/* Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-/* ou can also access individual characters in a string using bracket notation and the index. For instance, you can write:
-
-const hello = 'Hello World';
-console.log(hello[6]);
-// Output: W
-The console will display W since it is the character that is at index 6.
-
-
-const yomama ="banaaramadingdonglamadamalinglongwaawaawaawaadingalangdongmydangadongdingdong";
-console.log(yomama[Math.floor(Math.random() * yomama.length)]);
-const youpapa="123123123123";
-console.log(yopapa[Math.floor(Math.random() * yopapa.length)]);
-let conca=yomama.concat(yopapa);
-console.log(conca);
-//loop this one
-console.log(conca[Math.floor(Math.random() * conca.length)]);
-
-*/
